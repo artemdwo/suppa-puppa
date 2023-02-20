7f7e9193aa4d70254e51663ca46788237563a7a6
Artem <artemdwo@gmail.com>
2023-02-20 23:16:05 +0000

    feat: :thread: Base DB migration aka starting

---
+++ supabase/migrations/20230209220617_starting-ddl.sql
@@ -0,0 +1,27 @@
+create table user_profiles (
+  user_id uuid primary key references auth.users(id) not null,
+  username text unique not null,
+  CONSTRAINT proper_username CHECK (username ~* '^[a-zA-Z0-9_]+$'),
+  CONSTRAINT username_length CHECK (char_length(username) > 3 and char_length(username) < 15)
+);
+
+alter table user_profiles enable row level security;
+
+-- Anyone can see a user's profile
+CREATE POLICY "all can see" ON "public"."user_profiles"
+AS PERMISSIVE FOR SELECT
+TO public
+USING (true);
+
+-- Only the user can create (insert) their own profile details
+CREATE POLICY "users can insert" ON "public"."user_profiles"
+AS PERMISSIVE FOR INSERT
+TO public
+WITH CHECK (auth.uid() = user_id);
+
+-- Only the user can update their own profile details
+CREATE POLICY "owners can update" ON "public"."user_profiles"
+AS PERMISSIVE FOR UPDATE
+TO public
+USING (auth.uid() = user_id)
+WITH CHECK (auth.uid() = user_id);
